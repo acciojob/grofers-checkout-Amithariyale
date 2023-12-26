2de9cee5d0b735c5af6c87d8fbd6483b7e72601d
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2023-12-26 21:02:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	const prices=document.querySelectorAll('.price');
+	let sum=0;
+	for(let i=0;i<prices.length;i++){
+		sum+=parseInt(prices[i].innerText);
+	}
+
+	const tr=document.createElement('tr');
+	tr.colspan="2";
+	tr.innerText=sum;
+	tr.style.textAlign="right"
+	const table=document.querySelector('table');
+	table.append(tr);
   
 };
 
